3f488da85e6c26d18247cb7642107ce84fd9d887
Rrogntudju <mario.prince@outlook.com>
2019-06-15 14:22:24 -0400

    -S

--- locate/src/main.rs
+++ locate/src/main.rs
@@ -2,7 +2,9 @@ use {
         frcode::FrDecompress,
         std::env,
         std::fs::File,
+        std::io::Read,
         serde::Deserialize,
+        clap::{App, Arg},
 };
 
 macro_rules! unwrap {
@@ -27,5 +29,33 @@ struct Statistics {
 }
 
 fn main() {
-    println!("Hello, world!");
+    /*
+        -A, --all              only print entries that match all patterns
+        -b, --basename         match only the base name of path names
+        -c, --count            only print number of found entries 
+        -h, --help             print this help
+        -i, --ignore-case      ignore case distinctions when matching patterns
+        -l, --limit, -n LIMIT  limit output (or counting) to LIMIT entries
+        -S, --statistics       don't search for entries, print statistics about each used database
+        -r, --regexp REGEXP    search for basic regexp REGEXP instead of patterns
+            --regex            patterns are extended regexps
+        -w, --wholename        match whole path name (default)
+
+    */
+    let matches = App::new("locate")
+                    .arg(Arg::with_name("stats")
+                                .help("pas de recherche, affiche les statistiques de la base de données") 
+                                .short("S")                   
+                                .long("statistics")            
+                    )
+                    .get_matches();
+    
+
+    if matches.is_present("stats") {
+        let mut stat = env::temp_dir();
+        stat.push("locate");
+        stat.set_extension("txt");
+        let f = unwrap!(File::open(stat));
+        let stats = unwrap!(serde_json::from_reader(f));
+    }
 }
