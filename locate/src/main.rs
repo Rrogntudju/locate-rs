b479b872981a05b1ca587ffadbdbc23ca6ecf5aa
Rrogntudju <mario.prince@outlook.com>
2020-05-15 12:48:51 -0400

    version 6.2.3

--- locate/src/main.rs
+++ locate/src/main.rs
@@ -24,7 +24,7 @@ fn is_usize(v: String) -> Result<(), String> {
 
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = App::new("locate")
-        .version("0.6.2")
+        .version("0.6.3")
         .arg(
             Arg::with_name("stats")
                 .help("don't search for entries, print statistics about database")
