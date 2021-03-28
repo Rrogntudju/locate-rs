7cac6d1a03d66d90265470a0b129aed6b8e7790d
Rrogntudju <rrogntudju@example.com>
2021-03-28 12:02:35 -0400

    cute

--- locate/src/main.rs
+++ locate/src/main.rs
@@ -1,3 +1,4 @@
+#![feature(destructuring_assignment)]
 use {
     clap::{App, Arg},
     frcode::FrDecompress,
@@ -165,7 +166,7 @@ fn main() -> Result<(), Box<dyn Error>> {
             if is_dir {
                 continue; // no need to match on a dir entry
             } else  {
-                entry_test = entry.rsplit_once('\\').unwrap().1;   // basename
+                (_, entry_test) = entry.rsplit_once('\\').unwrap();   // basename
             }
         };
 
